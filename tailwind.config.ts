52b4363b235807959bb3690fc9832100cb05dc1a
Rocio Alcala <rro.alcala@gmail.com>
2024-04-24 20:22:19 -0300

    adding personalice theme to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,17 +4,48 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
       },
-    },
+      fontFamily: {
+        inconsolata: ["var(--font-inconsolata)"],
+        "source-sans-pro": ["Source Sans Pro"],
+        manrope: ["var(--font-manrope)"],
+        mulish: ["var(--font-mulish)"]
+      },
+      colors: {
+        "background-light": "#F5F4F3",
+        "background-dark": "#211F1A",
+        "primary-text-light": "#181714",
+        "primary-text-dark": "#B1AB9D",
+        "secondary-text-light": "#B8B2A6",
+        "secondary-text-dark": "#F1EFED",
+        "background-secondary-light": "#B8B2A6",
+        "background-secondary-dark": "#4C463C"
+      },
+      keyframes: {
+        "fade-out-top": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-50%)"
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0%)"
+          }
+        }
+      },
+      animation: {
+        "fade-out-top": "fade-out-top 1.8s"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
