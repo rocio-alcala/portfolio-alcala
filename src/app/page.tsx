f1d56d24d0aba064d67221166e4658934059d5d3
Rocio Alcala <rro.alcala@gmail.com>
2024-04-25 18:36:40 -0300

    fixing typo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <h1 className="text-3xl sm:text-5xl md:text-8xl">Hi, I'm Rocio.</h1>
         <div className="w-max">
           <h2 className="typetext border-primary-text-light dark:border-primary-text-dark">
-            I'm a front end developer
+            Im a front end developer
           </h2>
         </div>
         <div className="w-max">
