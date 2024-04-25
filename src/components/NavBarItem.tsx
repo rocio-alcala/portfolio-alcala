45fb07c73f1ccea4ea84566b587f867aa09e0c9f
Rocio Alcala <rro.alcala@gmail.com>
2024-04-25 12:07:32 -0300

    root layout

---
+++ src/components/NavBarItem.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren } from "react";
+
+interface NavBarItemProps {
+  onClick?: () => void;
+}
+
+export default function NavBarItem({
+  onClick,
+  children
+}: NavBarItemProps & PropsWithChildren) {
+  return (
+    <li
+      onClick={onClick}
+      className="hover:cursor-pointer m-2 p-1  relative after:absolute after:bottom-0 after:left-0 after:bg-secondary-text-light dark:after:bg-secondary-text-dark after:w-0 after:h-[2px] hover:after:w-full hover:after:transition-* hover:text-secondary-text-light dark:hover:text-secondary-text-dark transition-* duration-300 hover:after:duration-300"
+    >
+      {children}
+    </li>
+  );
+}
