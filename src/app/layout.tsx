0b43d75be22e1e9993bf57a030f73933be93f189
Rocio Alcala <rro.alcala@gmail.com>
2024-04-26 14:54:15 -0300

    adding link to social media

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,11 @@ import {
   BiLogoLinkedinSquare
 } from "react-icons/bi";
 import NavBar from "@/components/NavBar";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Rocio Alcala",
-  description: "Portfolio"
+  description: "Portfolio Rocio Alcala"
 };
 
 export default function RootLayout({
@@ -34,18 +35,24 @@ export default function RootLayout({
             Made with love <BiHeart size={25} />
           </div>
           <div className="flex gap-4">
-            <BiLogoGithub
-              size={25}
-              className="hover:text-secondary-text-light dark:hover:text-secondary-text-dark transition duration-500"
-            />
-            <BiLogoLinkedinSquare
-              className="hover:text-secondary-text-light dark:hover:text-secondary-text-dark transition duration-500"
-              size={25}
-            />
-            <BiLogoGmail
-              className="hover:text-secondary-text-light dark:hover:text-secondary-text-dark transition duration-500"
-              size={25}
-            />
+            <Link href="https://github.com/rocio-alcala">
+              <BiLogoGithub
+                size={25}
+                className="hover:text-secondary-text-light dark:hover:text-secondary-text-dark transition duration-500"
+              />
+            </Link>
+            <Link href="https://www.linkedin.com/in/rocio-alcala-784174185/">
+              <BiLogoLinkedinSquare
+                className="hover:text-secondary-text-light dark:hover:text-secondary-text-dark transition duration-500"
+                size={25}
+              />
+            </Link>
+            <Link href={"mailto:rro.alcala@gmail.com"}>
+              <BiLogoGmail
+                className="hover:text-secondary-text-light dark:hover:text-secondary-text-dark transition duration-500"
+                size={25}
+              />
+            </Link>
           </div>
         </footer>
       </body>
