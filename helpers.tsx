e01296a693d3d589ca475a9f1087946a0e675eea
Rocio Alcala <rro.alcala@gmail.com>
2024-04-29 16:28:43 -0300

    chore: cn function

--- helpers.tsx
+++ helpers.tsx
@@ -1,3 +1,9 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function isRunningOnClient() {
   return typeof window !== "undefined";
+}
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(...inputs));
 }
