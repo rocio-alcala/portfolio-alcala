7edff25eb30da61b7b07b7a34381565191fb55c4
Rocio Alcala <rro.alcala@gmail.com>
2024-05-02 15:06:45 -0300

    feat: responsive in projects page

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -17,10 +17,10 @@ interface ProjectCardProps {
 
 export default function ProjectCard({ project }: ProjectCardProps) {
   return (
-    <Link className="cursor-pointer" href={project.url}>
-      <div className="flex-col justify-center border-2 rounded-md p-3 m-2 h-86 w-80 hover:scale-105 transition-all duration-500">
-        <div className="flex justify-between">
-          <h3 className="text-xl font-bold">{project.name}</h3>{" "}
+    <Link className="cursor-pointer font-inconsolata" href={project.url}>
+      <div className="flex-col relative justify-center border-2 rounded-md p-3 m-2 h-86 w-80 hover:scale-105 transition-all duration-500">
+        <div className="flex justify-between ">
+          <h3 className="text-xl font-bold pb-2">{project.name}</h3>{" "}
           <Link href={project.repo}>
             <BiLogoGithub
               size={25}
@@ -28,8 +28,13 @@ export default function ProjectCard({ project }: ProjectCardProps) {
             />
           </Link>
         </div>
-        <p>{project.description}</p>
-        <img src={project.img} alt={project.name} />
+        <p className="pb-3 md:hidden">{project.description}</p>
+        <div className="relative">
+          <img className="rounded" src={project.img} alt={project.name} />
+          <p className="hidden md:block text-gray-900 rounded absolute top-0 left-0 h-full p-3 backdrop-blur-3xl opacity-0 hover:opacity-100 transition-all duration-700">
+            {project.description}
+          </p>
+        </div>
       </div>
     </Link>
   );
