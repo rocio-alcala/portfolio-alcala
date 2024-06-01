5eaed7840d3d1232f223d011b4726f377e53f2c9
Rocio Alcala <rro.alcala@gmail.com>
2024-05-31 21:51:55 -0300

    adding color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "selector",
   theme: {
@@ -12,14 +12,14 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
         inconsolata: ["var(--font-inconsolata)"],
         "source-sans-pro": ["Source Sans Pro"],
         manrope: ["var(--font-manrope)"],
         mulish: ["var(--font-mulish)"],
-        poppins: ["var(--font-poppins)"]
+        poppins: ["var(--font-poppins)"],
       },
       colors: {
         "background-light": "#F5F4F3",
@@ -29,93 +29,95 @@ const config: Config = {
         "secondary-text-light": "#B8B2A6",
         "secondary-text-dark": "#F1EFED",
         "background-secondary-light": "#DEDCD6",
-        "background-secondary-dark": "#4C463C"
+        "background-secondary-dark": "#4C463C",
+        "primary-color": "#D6A746",
+        "secondary-color": "#FFDF2B",
       },
       keyframes: {
         "fade-out-top": {
           "0%": {
             opacity: "0",
-            transform: "translateY(-50%)"
+            transform: "translateY(-50%)",
           },
           "100%": {
             opacity: "1",
-            transform: "translateY(0%)"
-          }
+            transform: "translateY(0%)",
+          },
         },
         "fade-out-bottom": {
           "0%": {
             opacity: "0",
-            transform: "translateY(50%)"
+            transform: "translateY(50%)",
           },
           "100%": {
             opacity: "1",
-            transform: "translateY(0%)"
-          }
+            transform: "translateY(0%)",
+          },
         },
         "fade-in": {
           "0%": {
-            opacity: "0"
+            opacity: "0",
           },
           "100%": {
-            opacity: "1"
-          }
+            opacity: "1",
+          },
         },
         "fade-in-right": {
           "0%": {
             opacity: "0",
-            transform: "translateX(-60px)"
+            transform: "translateX(-60px)",
           },
           "100%": {
             opacity: "1",
-            transform: "translateX(0)"
-          }
+            transform: "translateX(0)",
+          },
         },
         "fade-in-left": {
           "0%": {
             opacity: "0",
-            transform: "translateX(60px)"
+            transform: "translateX(60px)",
           },
           "100%": {
             opacity: "1",
-            transform: "translateX(0)"
-          }
+            transform: "translateX(0)",
+          },
         },
         pulse: {
           "0%": {
-            transform: "scale(1)"
+            transform: "scale(1)",
           },
           "50%": {
-            transform: "scale(1.4)"
+            transform: "scale(1.4)",
           },
           "100%": {
-            transform: "scale(1)"
-          }
+            transform: "scale(1)",
+          },
         },
         sink: {
           "0%": {
-            transform: "translateY(-10px)"
+            transform: "translateY(-10px)",
           },
           "50%": {
-            transform: "translateY(0)"
+            transform: "translateY(0)",
           },
           "100%": {
-            transform: "translateY(-10px)"
-          }
+            transform: "translateY(-10px)",
+          },
         },
         appear: {
           "0%": {
-            width: "0%"
+            width: "0%",
           },
           "100%": {
-            width: "100%"
-          }
+            width: "100%",
+          },
         },
         shadow: {
           "100%": {
             "box-shadow":
-              "0px 7px 10px rgba(0, 0, 0, -0.002) 0px 56px 80px rgba(0, 0, 0, 0.04)"
-          }
-        }
+              "0px 7px 10px rgba(0, 0, 0, -0.002) 0px 56px 80px rgba(0, 0, 0, 0.04)",
+          },
+        },
       },
       animation: {
         "fade-out-top": "fade-out-top 1.8s",
@@ -125,15 +127,15 @@ const config: Config = {
         "fade-in-left": "fade-in-left 1s ease-in-out",
         pulse: "pulse 1s ease-in-out",
         sink: "sink 1s ease-in-out infinite",
-        appear: "appear 1s ease"
-      }
-    }
+        appear: "appear 1s ease",
+      },
+    },
   },
   plugins: [
     require("tailwind-scrollbar")({
       nocompatible: true,
-      preferredStrategy: "pseudoelements"
-    })
-  ]
+      preferredStrategy: "pseudoelements",
+    }),
+  ],
 };
 export default config;
