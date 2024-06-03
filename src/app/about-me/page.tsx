206f2491e928e65bc9be2ec5585be3f81e2af3c0
Rocio Alcala <rro.alcala@gmail.com>
2024-06-03 15:12:47 -0300

    chore: suspense for Images

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import AboutMe from "@/components/Aboutme";
-import Loading from "@/components/LoadingBar";
-import { useDelay } from "@/hooks/useDelay";
 
 export default function AboutMePage() {
-  const isLoading = useDelay(1000);
-
-  return <> {isLoading ? <Loading /> : <AboutMe />}</>;
+  return (
+    <>
+      <AboutMe />
+    </>
+  );
 }
