dbb02043ee9bf3ca062b792b463f249c1cced58c
Rocio Alcala <rro.alcala@gmail.com>
2024-06-22 18:50:20 -0300

    changing typografy in titles

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from "@/components/ContactForm";
 export default function Contact() {
   return (
     <section className="mb-6 flex flex-1 animate-fade-in flex-col items-center justify-center p-10 font-inconsolata font-semibold">
-      <h1 className="font-bolder mb-5 text-start text-xl md:text-2xl">
+      <h1 className="mb-2 items-start py-2 text-start font-poppins text-4xl font-bold md:text-4xl">
         What do you want to tell me?
       </h1>
       <ContactForm />
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -5,10 +5,15 @@ export default async function Projects() {
   const projects = await getProjects();
 
   return (
-    <div className="flex flex-wrap justify-center gap-3">
-      {projects.map((project: Project) => (
-        <ProjectCard key={project.id} project={project} />
-      ))}
-    </div>
+    <>
+      <h2 className="mb-2 items-start py-2 text-start font-poppins text-4xl font-bold md:text-4xl">
+        Projects
+      </h2>
+      <div className="flex flex-wrap justify-center gap-3">
+        {projects.map((project: Project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </>
   );
 }
