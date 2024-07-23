4f6c4af7c5359aa6efc3dde457ea9a9938dccb3f
Rocio Alcala <rro.alcala@gmail.com>
2024-07-23 11:22:50 -0300

    style: loading bar

--- src/components/LoadingBar.tsx
+++ src/components/LoadingBar.tsx
@@ -1,6 +1,13 @@
-export default function LoadingBar() {
+import { cn } from "../../helpers";
+
+export default function LoadingBar({ className }: { className?: string }) {
   return (
-    <div className="m-32 flex flex-1 flex-col justify-center">
+    <div
+      className={cn(
+        "m-10 flex flex-1 flex-col justify-center md:m-32",
+        className,
+      )}
+    >
       <div className="h-1 animate-[appear_2s_ease_both_infinite] bg-primary-text-light dark:bg-primary-text-dark"></div>
     </div>
   );
