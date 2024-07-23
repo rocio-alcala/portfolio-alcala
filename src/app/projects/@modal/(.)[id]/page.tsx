6027615cbf54553f0a990c90f608246d81878b42
Rocio Alcala <rro.alcala@gmail.com>
2024-07-23 11:33:37 -0300

    fix on building

--- src/app/projects/@modal/(.)[id]/page.tsx
+++ src/app/projects/@modal/(.)[id]/page.tsx
@@ -1,10 +1,6 @@
 import BaseModal from "@/components/BaseModal";
 import LoadingBar from "@/components/LoadingBar";
 import ProjectPage from "@/components/ProjectPage";
-import ProjectPageSkeleton from "@/components/ProjectPageSkeleton";
-import Spinner from "@/components/Spinner";
-import { getProjects } from "@/services/getProjects";
-import { notFound } from "next/navigation";
 import React, { Suspense } from "react";
 
 export default async function Page({
